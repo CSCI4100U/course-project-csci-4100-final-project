33e540905bb635727e4bf92c4c46ec4c5a66e68c
BenceTakacs15 <bence.takacs@ontariotechu.net>
2022-11-02 13:18:54 -0400

    Added two basic tabs for showing movies and books

---
+++ project/lib/home_page.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'movies_view.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text("Final Project"),
+          bottom: const TabBar(
+            tabs: [
+              Tab(icon: Icon(Icons.movie)),
+              Tab(icon: Icon(Icons.book)),
+            ],
+          ),
+        ),
+        body: const TabBarView(
+          children: [
+            MoviesView(),
+            Text("Books tab"),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
