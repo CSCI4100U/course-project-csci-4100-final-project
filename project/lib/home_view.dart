81ddf3a92a718f760c098251449eabe81537e3a8
Sejal Shingal <sejal.shingal2001@gmail.com>
2022-11-09 21:30:43 -0500

    updated READme

--- project/lib/home_view.dart
+++ project/lib/home_view.dart
@@ -16,23 +16,23 @@ class HomeView extends StatefulWidget {
 }
 class _MyAppState extends State<HomeView> {
 
-  Future<Movie> fetchAlbum() async {
+  Future<List<Movie>> fetchAlbum() async {
     var response = await http
         .get(Uri.parse('https://api.themoviedb.org/3/trending/movie/day?api_key=3504ebf3ee269a0d7dbc3e0e586c0768')
     );
 
     if (response.statusCode == 200) {
-      print("getting data");
-      var parsedListJson = jsonDecode(response.body);
-      // print("$parsedListJson");
-      var results = parsedListJson['results'];
-      List <Movie> trending = [];
-      for(var m in results){
-        Movie 
-      }
+      var parsedListJson = jsonDecode(response.body)['results'];
+      print(parsedListJson);
 
-      return (Movie(results: "x", id: 123, title: "FFFF"));
-      // List<Movie> itemsList = List<Movie>.from(parsedListJson.map<Item>((dynamic i) => Item.fromJson(i)));
+      List<Movie> trending = [];
+      for (var m in parsedListJson.length) {
+        print("Item: $m");
+        Movie movie = Movie(results: parsedListJson[m]['release_date'], id: parsedListJson[m]['id'], title: parsedListJson[m]['title']);
+        trending.add(movie);
+      }
+      return trending;
+
     } else {
       throw Exception('Failed to load movies');
     }
@@ -41,15 +41,21 @@ class _MyAppState extends State<HomeView> {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: FutureBuilder<Movie>(
+      child: FutureBuilder<List<Movie>>(
         future: fetchAlbum(),
         builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            return Text(snapshot.data!.title);
-          } else if (snapshot.hasError) {
-            return Text('${snapshot.error}');
+          print(snapshot.data);
+          if (snapshot.data == null) {
+            return Text("Loading...");
           }
-
+          else {
+            return ListView.builder(
+                itemCount: snapshot.data?.length,
+                itemBuilder: (context, index){
+                  return Text(snapshot.data![index].title);
+            }
+            );
+          }
           // By default, show a loading spinner.
           return const CircularProgressIndicator();
         },
