2916dc50d007b4bcdc64d8b855debf86a0155d52
Sejal Shingal <sejal.shingal2001@gmail.com>
2022-12-05 15:10:02 -0500

    Added axis labels and title to chart

--- project/lib/views/chart_page.dart
+++ project/lib/views/chart_page.dart
@@ -13,20 +13,57 @@ class ChartPage extends StatefulWidget {
 }
 
 class _ChartPageState extends State<ChartPage> {
+  var avgRating = 0;
+  @override
+  void initState(){
+    _calculateFrequencies();
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Trending Stats"),
       ),
-      body: charts.BarChart(
-        [
-          charts.Series(
-            id: "Trending Ratings",
-            domainFn: (rf, _) => rf.rating,
-            measureFn: (rf, _) => rf.frequency,
-            data: _calculateFrequencies(),
+      body: Column(
+        children: [
+          SizedBox(
+            height: 500,
+            child: charts.BarChart(
+              [
+                charts.Series(
+                  id: "Trending Ratings",
+                  domainFn: (rf, _) => rf.rating,
+                  measureFn: (rf, _) => rf.frequency,
+                  data: _calculateFrequencies(),
+                ),
+              ],
+              animate: true,
+              behaviors: [
+                charts.ChartTitle('Ratings of Trending Movies',
+                    behaviorPosition: charts.BehaviorPosition.top,
+                    titleOutsideJustification: charts.OutsideJustification.start,
+                    innerPadding: 30),
+                charts.ChartTitle('Rating',
+                    behaviorPosition: charts.BehaviorPosition.bottom,
+                    titleOutsideJustification:
+                    charts.OutsideJustification.middleDrawArea),
+                charts.ChartTitle('Number of Movies',
+                    behaviorPosition: charts.BehaviorPosition.start,
+                    titleOutsideJustification:
+                    charts.OutsideJustification.middleDrawArea),
+              ],
+            ),
           ),
+          SizedBox(
+            height: 200,
+            child: Text(
+              "Average Rating of the Trending Movies: $avgRating",
+              style: const TextStyle(
+                fontFamily: "Lato",
+                fontSize: 20,
+              ),
+            ),
+          )
         ],
       )
     );
@@ -41,13 +78,15 @@ class _ChartPageState extends State<ChartPage> {
       "4": 0,
       "5": 0,
     };
-
+    var count =0;
     for (Trending<Movie> movie in widget.trending) {
       String rating = movie.rating.floor().toString();
       int oldFreq = frequencies[rating]!;
       frequencies[rating] = oldFreq + 1;
+      count = count + 1;
+      avgRating = avgRating + movie.rating.toInt();
     }
-
+    avgRating = avgRating~/count;
     return ["0", "1", "2", "3", "4", "5"].map((r) => RatingFrequency(rating: r, frequency: frequencies[r]!)).toList();
   }
 }
