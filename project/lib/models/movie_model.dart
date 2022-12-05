ffd07a6215afc3f77ce1d88c496741c2bff6042b
BenceTakacs15 <59862020+BenceTakacs15@users.noreply.github.com>
2022-12-04 20:48:14 -0500

    Movie list is no longer global and is now attached to each user

--- project/lib/models/movie_model.dart
+++ project/lib/models/movie_model.dart
@@ -3,13 +3,18 @@ import 'package:project/classes/movie.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:project/models/fetch_data.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class MoviesModel {
   Future<List<Movie>> getAllMovies() async {
     List<Movie> movies = [];
 
     print("Requesting data from Firestore");
-    var collection = await FirebaseFirestore.instance.collection("movieList").get();
+    if (FirebaseAuth.instance.currentUser == null) {
+      print("Not logged in");
+      return [];
+    }
+    var collection = await FirebaseFirestore.instance.collection("movieList").where("user_id", isEqualTo: FirebaseAuth.instance.currentUser!.uid).get();
     print("Successfully retrieved data from Firestore");
     for (var doc in collection.docs) {
       //Movie movie = Movie.fromMap(doc.data());
