8d81dc58c255e06130bc3829d5a9b9cc8542f3e0
BenceTakacs15 <59862020+BenceTakacs15@users.noreply.github.com>
2022-12-05 19:24:11 -0500

    Clicking on a search suggestion now does the same thing as the result

--- project/lib/models/movie_search_delegate.dart
+++ project/lib/models/movie_search_delegate.dart
@@ -49,28 +49,6 @@ class MovieSearchDelegate extends SearchDelegate<Movie?> {
 
   @override
   Widget buildSuggestions(BuildContext context) {
-    return FutureBuilder<List<Movie>>(
-        future: Fetch.fetchMoviesFromSearchQuery(query),
-        builder: (context, snapshot) {
-          if (!snapshot.hasData) {
-            return const Center(child: CircularProgressIndicator());
-          }
-          return ListView.builder(
-              itemCount: snapshot.data!.length,
-              itemBuilder: (context, index) {
-                return GestureDetector(
-                  child: MovieTile(movie: snapshot.data![index]),
-                  onTap: () {
-                    if (query == snapshot.data![index].title) {
-                      close(context, snapshot.data![index]);
-                    } else {
-                      query = snapshot.data![index].title;
-                    }
-                  }
-                );
-              }
-          );
-        }
-    );
+    return buildResults(context);
   }
 }
